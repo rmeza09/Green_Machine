3bfa6dcb22674aba029658a98a56273b18dcae28
rmeza09 <rafasmez1009@gmail.com>
2024-08-17 15:48:59 -0400

    created 08/17/2024

---
+++ Main.py
@@ -0,0 +1,17 @@
+
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import numpy as np
+import csv
+import pandas
+
+AAPLdata = pandas.read_csv(r"C:\\Users\\rmez1\\OneDrive\\Desktop\\StockAnalysis\\02 - Data\\AAPL.csv")
+
+print(AAPLdata[1])
+
+#x = np.linspace(0, 20, 100)
+#plt.plot(x, np.sin(x))
+#plt.show()
+
+
+
