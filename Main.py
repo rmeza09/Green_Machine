7a0935741007d4aa9061e4d557812311e5f574e3
XxbecharaxX <josephanthonybechara@gmail.com>
2024-08-17 22:38:30 -0400

    update 081724

--- Main.py
+++ Main.py
@@ -1,14 +1,16 @@
 
 import matplotlib.pyplot as plt
 import matplotlib as mpl
+import pandas as pd
 import numpy as np
 import csv
-import pandas
 
-AAPLdata = pandas.read_csv(r"C:\\Users\\rmez1\\OneDrive\\Desktop\\StockAnalysis\\02 - Data\\AAPL.csv")
+
+AAPLdata = pd.read_csv(r"C:\\Users\\rmez1\\OneDrive\\Desktop\\StockAnalysis\\02 - Data\\AAPL.csv")
 
 print(AAPLdata[1])
-
+#Helo
+#panda
 #x = np.linspace(0, 20, 100)
 #plt.plot(x, np.sin(x))
 #plt.show()
