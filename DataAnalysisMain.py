e8274813069a136e5111bd4cda4fd595413bb705
rmeza09 <rafasmez1009@gmail.com>
2024-08-19 23:59:14 -0400

    update 08/19/2024

--- DataAnalysisMain.py
+++ DataAnalysisMain.py
@@ -8,7 +8,7 @@ import pandas
 AAPLdata = pandas.read_csv(r"C:\\Users\\rmez1\\OneDrive\\Desktop\\StockAnalysis\\02 - Data\\AAPL.csv")
 
 print(AAPLdata[1])
-
+#sorry
 #x = np.linspace(0, 20, 100)
 #plt.plot(x, np.sin(x))
 #plt.show()
