65cefcd3e38c068ca29ef2c32315b208e94c776a
rmeza09 <rafasmez1009@gmail.com>
2025-04-08 01:31:45 -0400

    created base files for experimenting with equities/crypto

---
+++ DataAnalysisMain.py
@@ -0,0 +1,52 @@
+
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import numpy as np
+import csv
+import pandas as pd
+#import mplfinance as mpf
+
+dataSheet = pd.read_csv(r"C:\\Users\\rmez1\\OneDrive\\Desktop\\Green_Machine\\02 - Data\\AAPL.csv")
+
+dataSheet['Date'] = pd.to_datetime(dataSheet['Date'])
+dataSheet.drop('Adj Close', axis=1, inplace=True)
+
+
+plt.figure(1, figsize=(10, 6))
+plt.plot(dataSheet['Date'], dataSheet['Close'], label = 'Close Price')
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.title('Stock Close Prices Over Time')
+plt.legend()
+plt.grid(True)
+
+
+plt.figure(2, figsize=(10, 6))
+plt.plot(dataSheet['Date'], dataSheet['Open'], labe = 'Open Price')
+plt.plot(dataSheet['Date'], dataSheet['Close'], label = 'Close Price')
+plt.xlabel('Date')
+plt.ylabel('Price')
+plt.title('Stock Open and Close Prices Over Time')
+plt.legend()
+plt.grid(True)
+
+
+plt.figure(3, figsize=(10, 6))
+plt.bar(dataSheet['Date'], dataSheet['Volume'], label = 'Volume', color='orange')
+plt.xlabel('Date')
+plt.ylabel('Volume')
+plt.title('Trading Volume Over Time')
+plt.legend()
+plt.grid(True)
+
+
+plt.show()
+
+
+#sorry
+#x = np.linspace(0, 20, 100)
+#plt.plot(x, np.sin(x))
+#plt.show()
+
+  
+
